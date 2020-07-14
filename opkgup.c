e11c7add8ce4d680a275d94d565fa3ffcd09fa41
mbsd <mbsd@m0x.ru>
2020-07-14 16:56:51 -0300

    simulate compare packages

--- opkgup.c
+++ opkgup.c
@@ -15,20 +15,64 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int
 main(int argc, char *argv[])
 {
-	const char 	*pkgv1 = "123";
-	const char 	*pkgv2 = "124";
+	int 		i = 0;
+	int 		j = 0;
+	/* pacote 1 (antigo) */
+	const char 	*pkgv1 = "pacote1-123";
+	/* pacote 2 (atualizado) */
+	const char 	*pkgv2 = "pacote1-124";
+	/* pacote 3 */
+	const char	*pkgv3 = "pacote2-123";
+	/* lista de pacotes do index.txt */
+	char		*pacotes_ind[20000];
+	/* lista de pacotes instalados */
+	char		*pacotes_ins[20000];
 
 	printf("opkgup\n");
 
-	if (strcmp(pkgv1, pkgv2) == 0)
-		printf("%s == %s\n", pkgv1, pkgv1);
-	else
-		printf("%s != %s (precisamos olhar melhor)\n", pkgv1, pkgv2);
+	memset(pacotes_ins, '\0', sizeof(pacotes_ind));
+	memset(pacotes_ind, '\0', sizeof(pacotes_ind));
+	
+	/* inserimos os pacotes do index.txt */
+	pacotes_ind[0] = strdup(pkgv1);
+	pacotes_ind[1] = strdup(pkgv2);
+	pacotes_ind[2] = strdup(pkgv3);
+
+	/* inserimos os pacotes instalados (troquei a ordem ) */
+	pacotes_ins[0] = strdup(pkgv2);
+	pacotes_ins[1] = strdup(pkgv1);
+	pacotes_ins[2] = strdup(pkgv3);
+
+	/* iteramos na lista de pacotes do index.txt */
+	for (i = 0; pacotes_ind[i] != NULL; i++) {
+
+		/* iteramos na lista de pacotes instalados */
+		for (j = 0; pacotes_ins[j] != NULL; j++) {
+			/* comparamos as versoes obtidas */
+			if (strcmp(pacotes_ind[i], pacotes_ins[j]) == 0)
+				printf("%s == %s\n", pacotes_ind[i], 
+				    pacotes_ins[j]);
+		else
+				printf("%s != %s\n", pacotes_ind[i],
+				    pacotes_ins[j]);
+		}
+	}
+
+	/* liberamos os nomes de pacotes index.txt */
+	free(pacotes_ind[0]);
+	free(pacotes_ind[1]);
+	free(pacotes_ind[2]);
+
+	/* liberamos os nomes de pacotes instalados */
+	free(pacotes_ins[0]);
+	free(pacotes_ins[1]);
+	free(pacotes_ins[2]);
 
 	return (0);
 }
