0ab0061b9666bbd625c0ed3dfd5575b55d1822f9
mbsd <mbsd@m0x.ru>
2020-07-18 17:45:13 -0300

    list packages

--- opkgup.c
+++ opkgup.c
@@ -14,108 +14,139 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+#include <sys/queue.h>
+
 #include <ctype.h>
 #include <dirent.h>
+#include <err.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <err.h>
 
-static char *get_pkg_vs(const char *);
-static char *get_pkg_name(const char *);
+#define EXT "tgz"
+#define INDEX "index.txt"
+
+/*
+ * package
+ */
+typedef struct pkg {
+	char 			*fullname;
+	char 			*name;
+	char 			*ver;
+
+	SLIST_ENTRY(pkg) 	 entries;
+} pkg_t;
+
+typedef struct opkg {
+	char 				*arch;
+	char 				*pkg_path;
+	SLIST_HEAD(package_dir, pkg) 	 pkgd;
+	SLIST_HEAD(package_index, pkg) 	 pkgi;
+} opkg_t;
+
+static pkg_t	*pkg_new(const char *);
+static void 	 pkg_free(pkg_t *);
+static void	 pkg_print(pkg_t *);
+static char	*pkg_get_ver(const char *);
+static char	*pkg_get_name(const char *);
+static int	 ext_exist(const char *);
+opkg_t		*opkg_new(void);
+void		 opkg_free(opkg_t *);
+int		 opkg_push_pkg(opkg_t *, const char *);
 
 int
 main(int argc, char *argv[])
 {
-	/* lista de pacotes do index.txt */
-	char		*pacotes_ind[20000];
-	/* lista de pacotes instalados */
-	char		*pacotes_ins[20000];
-	/* diretorio onde fica os pacotes info dos pacotes */
-	DIR 		*pkgdb = NULL;
-	/* estrutura de um arquivo */
-	struct	dirent	*dp = NULL;
-	int i = 0;
-	FILE *fp = NULL;
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t read = 0;
+	pkg_t 	*p = NULL;
+	pkg_t 	*pn = NULL;
+	opkg_t 	*o = opkg_new();
 
-	printf("opkgup\n");
+	if (opkg_push_pkg(o, "pacote-1.0.0p1.tgz") != 0)
+		goto clean;
 
-	/* inicializamos limpando os vetores de pacotes */
-	memset(pacotes_ins, '\0', sizeof(pacotes_ind));
-	memset(pacotes_ind, '\0', sizeof(pacotes_ind));
-
-	/* abrimos a pasta */
-	pkgdb = opendir("/var/db/pkg");
-
-	if (pkgdb == NULL)
-		err(1, "pkgdb null");
-
-	/* iteramos no diretorio */
-	while ((dp = readdir(pkgdb)) != NULL) {
-		/* temos que ignoramos alguns pacotes */
-		/* TODO: strings vazias tb temos que ignorar */
-		if (dp->d_name[0] != '.' && strstr(dp->d_name, "firmware") == NULL) {
-			const char *pkgf= dp->d_name;
-			/* obtenhho a versão */
-			const char *vs = get_pkg_vs(pkgf);
-			/* obtenho o nome */
-			char *name = get_pkg_name(pkgf);
-			
-			printf("Pacote: %s Versão: %s\n", name, vs);
-
-			free(name);
-			i++;
-		}
-		/*
-		if (i == 10)
-			break;
-		*/
+	for (p = SLIST_FIRST(&o->pkgd); p != NULL; p = pn) {
+		pn = SLIST_NEXT(p, entries);
+		pkg_print(p);
 	}
 
-	/* abrir o arquivo index.txt */
-	fp = fopen("./index.txt", "r");
-	if (fp == NULL) {
-		closedir(pkgdb);
-		err(1, "fopen");
-	}
-
-	while ((read = getline(&line, &len, fp)) != -1) {
-		/* removemos \n */
-		line[read-1] = '\0';
-		/* procuro pelo ultimo espaco */
-		char *l = strrchr(line, ' ');
-		/* pulo para o endereco */
-		l++;
-		/* obter a versao */
-		const char *vrs = get_pkg_vs(l);
-		char *name = get_pkg_name(l);
-
-		printf("Pacote: %s\t\t\t\tVersão: %s\n", name, vrs);
-
-		free(name);
-
-	}
-
-	/* fecho o descritor */
-	fclose(fp);
-	/* limpo a linha caso ela exista */
-	if (line != NULL) free(line);
-
-	closedir(pkgdb);
+clean:
+	opkg_free(o);
 
 	return (0);
 }
 
+/*
+ * TODO: option to push multiple packages type
+ */
+int
+opkg_push_pkg(opkg_t *o, const char *n)
+{
+	pkg_t *pf = NULL;
+	pkg_t *p = pkg_new(n);
+
+	if (p == NULL)
+		return (-1);
+
+	if (SLIST_EMPTY(&o->pkgd))
+		SLIST_INSERT_HEAD(&o->pkgd, p, entries);
+	else {
+		pf = SLIST_FIRST(&o->pkgd);
+		SLIST_INSERT_AFTER(pf, p, entries);
+	}
+	return (0);
+}
+
+opkg_t *
+opkg_new(void)
+{
+	opkg_t *o = NULL;
+
+	o = malloc(sizeof(o));
+	if (o == NULL)
+		err(2, "malloc");
+
+	o->arch = NULL;
+	o->pkg_path = NULL;
+
+	SLIST_INIT(&o->pkgd);
+	SLIST_INIT(&o->pkgi);
+
+	return (o);
+}
+
+void
+opkg_free(opkg_t *o)
+{
+	pkg_t *p = NULL;
+	pkg_t *pn = NULL;
+
+	if (o == NULL)
+		return;
+	if (o->arch != NULL)
+		free(o->arch);
+	if (o->pkg_path != NULL)
+		free(o->pkg_path);
+
+	for (p = SLIST_FIRST(&o->pkgd); p != NULL; p = pn) {
+		pn = SLIST_NEXT(p, entries);
+		pkg_free(p);
+	}
+
+	for (p = SLIST_FIRST(&o->pkgi); p != NULL; p = pn) {
+		pn = SLIST_NEXT(p, entries);
+		pkg_free(p);
+	}
+
+	free(o);
+}
+
 static char *
-get_pkg_name(const char *st)
+pkg_get_name(const char *st)
 {
 	char *copy;
+	const char *s = st;
 	int c = 0;
 	size_t siz;
-	const char *s = st;
 
 	siz = strlen(s) + 1;
 	if ((copy = malloc(siz)) == NULL)
@@ -137,7 +168,7 @@ get_pkg_name(const char *st)
 }
 
 static char *
-get_pkg_vs(const char *s)
+pkg_get_ver(const char *s)
 {
 	char *save;
 
@@ -146,6 +177,74 @@ get_pkg_vs(const char *s)
 			save = (char *)s;
 		if (!*s)
 			return (save);
-
 	}
+}
+
+/*
+ * The ext_exist test if 'tgz' extension exist.
+ * return zero if extension not exist or non-zero if exist
+ */
+static int
+ext_exist(const char *fullname)
+{
+	const char *sp;
+
+	if ((sp = strrchr(fullname, '.')) == NULL)
+		return (0);
+	sp++;
+
+	if (strcmp(EXT, sp) == 0)
+		return (1);
+	return (0);
+}
+
+static void
+pkg_print(pkg_t *p)
+{
+	if (p == NULL) return;
+
+	printf("===========================================\n");
+	printf("Name:\t\t%s\nVersion:\t%s\nFullName:\t%s\n", p->name != NULL ?
+	    p->name : "", p->ver != NULL ? p->ver : "", p->fullname != NULL ?
+	    p->fullname : "");
+	printf("===========================================\n");
+}
+
+static pkg_t *
+pkg_new(const char *f)
+{
+	pkg_t *p = NULL;
+	size_t pos = strlen(f);
+
+	p = (pkg_t *)malloc(sizeof(pkg_t));
+	if (p == NULL)
+		err(2, "malloc");
+
+	if (f == NULL) {
+		free(p);
+		err(2, "fullname null");
+	}
+
+	/* TODO: checking is null */
+	p->fullname = strdup(f);
+
+	if ((ext_exist(f)) == 1)
+		p->fullname[pos-4] = '\0';
+
+	p->name = pkg_get_name(p->fullname);
+	p->ver = pkg_get_ver(p->fullname);
+
+	return (p);
+}
+
+static void
+pkg_free(pkg_t *p)
+{
+	if (p == NULL) return;
+
+	if (p->fullname != NULL)
+		free(p->fullname);
+	if (p->name != NULL)
+		free(p->name);
+	free(p);
 }
